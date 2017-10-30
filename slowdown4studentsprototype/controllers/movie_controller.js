2cf49125aadeaf1ae70a478821f38a6c0f4690f3
piachristel <muellerchristina@bluewin.ch>
2017-10-30 21:53:45 +0100

    Final version for mid-term presentation.

--- slowdown4studentsprototype/controllers/movie_controller.js
+++ slowdown4studentsprototype/controllers/movie_controller.js
@@ -4,7 +4,7 @@ var movies // Stores movie activites.
 // Exports makes index method accessible for other files, e.g. movie_routes.js in the route folder.
 // Lists all available movie activities. Renders the movies.hbs view.
 exports.index = function(req, res){
-    db.query("SELECT name, category FROM movie", function(error, rows, fields) {
+    db.query("SELECT name, category, place, date FROM movie", function(error, rows, fields) {
         if (error) {
             console.log('Error in query in movie retrieval');
         }
@@ -22,15 +22,19 @@ exports.index = function(req, res){
 
 // Renderes add_movie.hbs, which is a form for posting a new movie activity.
 exports.add = function(req, res) {
-    res.render('add_movie');
+    res.render('create_movie');
 };
 
 // Saves the post of the user and redirects to the list of all movies.
 exports.save = function(req, res) {   
     var input = JSON.parse(JSON.stringify(req.body));
     var data = { 
+        title: input.title,
         name: input.name,
-        title: input.title      
+        category: input.category,
+        place: input.place,
+        date: input.date    
+
     };
     var query = db.query("INSERT INTO movie set ? ",data, function(err, rows){
             if (err){
